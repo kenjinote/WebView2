dc34b2862c80372e323207ef6fe73736ce4b603a
kenjinote <2605401+kenjinote@users.noreply.github.com>
2021-12-04 23:33:00 +0900

    プロジェクト ファイルを追加します。

---
+++ Source.cpp
@@ -0,0 +1,118 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+#include <wrl.h>
+#include <wil/com.h>
+#include "WebView2.h"
+
+TCHAR szClassName[] = TEXT("Window");
+using namespace Microsoft::WRL;
+static wil::com_ptr<ICoreWebView2Controller> webviewController;
+static wil::com_ptr<ICoreWebView2> webviewWindow;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CREATE:
+		SendMessage(hWnd, WM_APP, 0, 0);
+		break;
+	case WM_SIZE:
+		if (webviewController != nullptr)
+		{
+			RECT bounds;
+			GetClientRect(hWnd, &bounds);
+			webviewController->put_Bounds(bounds);
+		}
+		break;
+	case WM_NCCREATE:
+		{
+			const HMODULE hModUser32 = GetModuleHandle(TEXT("user32.dll"));
+			if (hModUser32)
+			{
+				typedef BOOL(WINAPI*fnTypeEnableNCScaling)(HWND);
+				const fnTypeEnableNCScaling fnEnableNCScaling = (fnTypeEnableNCScaling)GetProcAddress(hModUser32, "EnableNonClientDpiScaling");
+				if (fnEnableNCScaling)
+				{
+					fnEnableNCScaling(hWnd);
+				}
+			}
+		}
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPWSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("WebView2"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+		Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+			[hWnd](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+				// Create a CoreWebView2Controller and get the associated CoreWebView2 whose parent is the main window hWnd
+				env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+					[hWnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+						if (controller != nullptr) {
+							webviewController = controller;
+							webviewController->get_CoreWebView2(&webviewWindow);
+						}
+						// Add a few settings for the webview
+						// The demo step is redundant since the values are the default settings
+						ICoreWebView2Settings* Settings;
+						webviewWindow->get_Settings(&Settings);
+						Settings->put_IsScriptEnabled(TRUE);
+						Settings->put_AreDefaultScriptDialogsEnabled(TRUE);
+						Settings->put_IsWebMessageEnabled(TRUE);
+						// Resize the WebView2 control to fit the bounds of the parent window
+						RECT bounds;
+						GetClientRect(hWnd, &bounds);
+						webviewController->put_Bounds(bounds);
+						// Schedule an async task to navigate to Bing
+						webviewWindow->Navigate(L"https://www.bing.com/");
+						// 4 - Navigation events
+						// 5 - Scripting
+						// 6 - Communication between host and web content
+						return S_OK;
+					}).Get());
+				return S_OK;
+			}).Get());
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
